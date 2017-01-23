53df41b73c07f81bdec7c5e25c32d530713fc165
coneofshame <popama052000@cvh.rusd.us>
2017-01-23 10:16:42 -0800

    double user input

--- Functions.py
+++ Functions.py
@@ -9,3 +9,9 @@ print(double(6))
 """2.  Write a program that will (1) ask the user for an input value, (2) take
 that and double it and  (3) print the result.
 Include necessary print statements and address whitespace issues."""
+def doubleuser():
+    number = int(input('Give me a number: '))
+    number = double(number)
+    return number
+
+print(doubleuser())
