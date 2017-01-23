53144b38256c9d52efe827ceb8d2a7b9f19d8d05
coneofshame <popama052000@cvh.rusd.us>
2017-01-23 10:31:39 -0800

    count by 2

--- Range.Function.py
+++ Range.Function.py
@@ -1,11 +1,14 @@
 """Use the range function to print the numbers from 1-20"""
 def count():
     for i in range(1, 21):
-        return i
+        print(i)
 
 print(count())
 """Repeat the exercise above counting by 2's"""
-
+def count2():
+    for i in range (2, 21, 2):
+        print(i)
+print(count2())
 
 
 """Print all the multiples of 5 between 10 and 200 in DECENDING order"""
