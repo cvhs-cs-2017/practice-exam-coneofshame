1da7594e274e9a436653b857b247202dcd4aedec
coneofshame <popama052000@cvh.rusd.us>
2017-01-23 10:14:06 -0800

    double

--- Functions.py
+++ Functions.py
@@ -1,8 +1,10 @@
 """1. Write a function that will double any integer (n) and return the result"""
+def double(number):
+    doub = number * 2
+    return doub
 
 
-
-
+print(double(6))
 
 """2.  Write a program that will (1) ask the user for an input value, (2) take
 that and double it and  (3) print the result.
