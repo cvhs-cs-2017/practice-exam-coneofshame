57ff59bbc32faaf50dafbe91e9115aa1d3917faa
coneofshame <popama052000@cvh.rusd.us>
2017-01-23 10:12:49 -0800

    done with encryption

--- Encrypt.py
+++ Encrypt.py
@@ -1,2 +1,20 @@
 """Write a code that will take (AnyString) and substitute all vowels with
 character's like $#%^&* etc..... but DO NOT USE ' or " as any of your characters."""
+def encrypt(plaintext):
+    plaintext= plaintext.lower()
+    ciphertext = ""
+    for ch in plaintext:
+        if ch == "a":
+            ciphertext = ciphertext + "$"
+        elif ch == 'e':
+            ciphertext = ciphertext + '#'
+        elif ch == 'i':
+            ciphertext = ciphertext + '%'
+        elif ch == 'o':
+            ciphertext = ciphertext + '^'
+        elif ch == 'u':
+            ciphertext = ciphertext + '&'
+        else:
+            ciphertext = ciphertext + ch
+    return ciphertext
+print(encrypt('HEllo how are you'))
