218275606e3eedaf456cae7598d42ff5e1b99d51
coneofshame <popama052000@cvh.rusd.us>
2017-01-23 10:28:47 -0800

    count

--- Range.Function.py
+++ Range.Function.py
@@ -1,7 +1,9 @@
 """Use the range function to print the numbers from 1-20"""
+def count():
+    for i in range(1, 21):
+        return i
 
-
-
+print(count())
 """Repeat the exercise above counting by 2's"""
 
 
